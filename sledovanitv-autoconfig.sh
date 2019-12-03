dbb4bdfef2c75a3c48d320269fb601c6b04972cc
petr.krejci@it-g.cz <petr.krejci@it-g.cz>
2019-12-03 16:49:18 +0100

    refaktoring playlistu, nyni primo generuje jq

--- sledovanitv-autoconfig.sh
+++ sledovanitv-autoconfig.sh
@@ -4,13 +4,4 @@ source $(dirname $0)/sledovanitv-token.sh
 
 # Vytvarim novy autoconfigfile
 echo "#EXTM3U"
-IFS=$'\n'
-for channel in $(wget -q -O- "https://sledovanitv.cz/api/playlist?PHPSESSID=${PHPSESSID}&quality=40&capabilities=h265%2Cadaptive"  | jq  -r '.channels[] | select (.locked=="none" and .type=="tv") | [.id,.name,.logoUrl,.url] | @text'); do
-	id=$(echo $channel | jq -r '.[0]')
-	label=$(echo $channel | jq -r '.[1]')
-	icon=$(echo $channel | jq -r '.[2]')
-	url=$(echo $channel | jq -r '.[3]')
-	
-	echo '#EXTINF:0 tvg-id="'${id}'" epg-id="'${id}'" tvg-name="'${label}'" tvg-logo="'${icon}'",'${label}
-	echo 'pipe://ffmpeg -nostats -loglevel 0 -protocol_whitelist "https,tls,http,file,tcp" -i "'${url}'" -c copy -map 0  -f mpegts pipe:1'
-done
+wget -q -O- "https://sledovanitv.cz/api/playlist?PHPSESSID=${SLEDOVANITVID}&quality=40&capabilities=h265%2Cadaptive"  | jq  -r '.channels[] | select (.locked=="none" and .type=="tv") | "#EXTINF:0 tvg-id=\"\(.id)\" epg-id=\"\(.id)\" tvg-name=\"\(.name)\" tvg-logo=\"\(.logoUrl)\",\(.name)\npipe://ffmpeg -nostats -loglevel 0 -protocol_whitelist \"https,tls,http,file,tcp\" -i \"\(.url)\" -c copy -map 0  -f mpegts pipe:1"'
--- sledovanitv-register.sh
+++ sledovanitv-register.sh
@@ -5,7 +5,9 @@ dir=$(dirname $0)
 read -p "SledovaniTV.cz email:" weblogin
 read -s -p "Heslo:" webpasswd
 echo
-regaddress=https://sledovanitv.cz/api/create-pairing?username=${weblogin}\&password=${webpasswd}\&type=xbmc\&product=$(hostname)\&serial=00:00:00:00:00:00
+
+mac=$(( find /sys/class/net -mindepth 1 -maxdepth 1 ! -name lo -execdir cat {}/address \; )  | head -n 1 )
+regaddress=https://sledovanitv.cz/api/create-pairing?username=${weblogin}\&password=${webpasswd}\&type=xbmc\&product=$(hostname)\&serial=${mac}
 regstring=$(wget -q -O - --no-check-certificate ${regaddress})
 echo "${regstring}" | grep -q -E "\"status\":1"
 if [ $? -gt 0 ]; then
@@ -21,7 +23,7 @@ if [ ! -s ${dir}/config.json ]; then
     echo "{}" > ${dir}/config.json
 fi
 
-cat ${dir}/config.json | jq '.device += {"id":"'${id}'","password":"'${password}'"}' > ${tmp}
+cat ${dir}/config.json | jq '.device += {"id":"'${id}'","password":"'${password}'","serial":"'${mac}'"}' > ${tmp}
 cat ${tmp} > ${dir}/config.json
 rm ${tmp}
 
