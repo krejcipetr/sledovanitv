ccbac56663bfe51de07aa4bd924c39172b3b392f
petr.krejci@it-g.cz <petr.krejci@it-g.cz>
2019-12-23 09:03:22 +0100

    doplneni manualniho nastaveni service_name

--- sledovanitv-autoconfig.sh
+++ sledovanitv-autoconfig.sh
@@ -21,7 +21,7 @@ fi
 
 # Vytvarim novy playlist
 echo "#EXTM3U"
-echo $playlist | jq -r  '.channels | to_entries[] | select ((.value.locked=="none" or .value.locked=="'${lockedpin}'") and .value.type=="tv") | "#EXTINF:-1 tvg-chno=\"\(.key+1)\" tvg-id=\"\(.value.id)\" epg-id=\"\(.value.id)\" tvg-name=\"\(.value.name)\" tvg-logo=\"\(.value.logoUrl)\"  group-title=\"${SLEDOVANITVGRP\(.value.group)}\",\(.value.name)\npipe://'$(dirname $(realpath $0) )'/sledovanitv-playback.sh \"\(.value.url | sub("PHPSESSID=[0-9a-z]+";"PHPSESSID=%PHPSESSID%") )\""' > ${FILETMP}
+echo $playlist | jq -r  '.channels | to_entries[] | select ((.value.locked=="none" or .value.locked=="'${lockedpin}'") and .value.type=="tv") | "#EXTINF:-1 tvg-chno=\"\(.key+1)\" tvg-id=\"\(.value.id)\" epg-id=\"\(.value.id)\" tvg-name=\"\(.value.name)\" tvg-logo=\"\(.value.logoUrl)\"  group-title=\"${SLEDOVANITVGRP\(.value.group)}\",\(.value.name)\npipe://'$(dirname $(realpath $0) )'/sledovanitv-playback.sh \"\(.value.url | sub("PHPSESSID=[0-9a-z]+";"PHPSESSID=%PHPSESSID%") )\" \"\(.value.name)\""' > ${FILETMP}
 sed -i -E 's/["#&()]/\\\\\0/g' ${FILETMP}
 
 # Vypis playlist a nahrad v nem nazvy skupin
