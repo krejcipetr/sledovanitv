ac4f052b28973d8bb8160c1782ca81506a00b3e7
Petr Krejci <petr.krejci@aura.cz>
2020-01-29 14:50:06 +0100

    oprava zmeny formatu linku. playlist vraci neni "." po nazvu kanalu a url neni validni

--- sledovanitv-playback.sh
+++ sledovanitv-playback.sh
@@ -3,4 +3,4 @@
 # Pouzite parametry jsou nasledujici:
 source `dirname $0`/sledovanitv-token.sh
 
-ffmpeg -nostats -loglevel 0 -protocol_whitelist "https,http,tls,file,tcp" -i "$(echo ${1} | sed -E 's/%PHPSESSID%/'${SLEDOVANITVID}'/g')" -c copy -map 0  -f mpegts -metadata service_provider=SledovaniTV -metadata service_name=${2}  pipe:1
+ffmpeg -protocol_whitelist "https,http,tls,file,tcp" -i "$(echo ${1} | sed -E 's/%PHPSESSID%/'${SLEDOVANITVID}'/g')" -c copy -map 0  -f mpegts -metadata service_provider=SledovaniTV -metadata service_name="${2}"  pipe:1
