c963ed2b119db291a9779305bc52308cbfdee0c6
Petr Krejčí <petr.krejci@it-g.cz>
2023-01-29 15:40:21 +0100

    Reseni problemu s FILETMP souborem

--- sledovanitv-autoconfig.sh
+++ sledovanitv-autoconfig.sh
@@ -2,7 +2,26 @@
 
 source $(dirname $0)/sledovanitv-token.sh
 
-FILETMP=${cachedir}/sledovanitv_playlist
+FILETMP=${cachedir}/sledovanitv/playlist
+
+if [ -s ${FILETMP} ]; then
+        file_time=$(( $(stat -t ${FILETMP} | cut -d" " -f 13) ))
+        current_time=$(date +%s)
+
+        # Stari max 8h
+        stari=$(( current_time - 60 * 60 * 8 ))
+
+        if [ ${file_time} -gt ${stari} ]; then
+                cat  ${FILETMP}
+		exit
+        fi
+fi
+
+echo "#EXTM3U" > ${FILETMP}
+
+if [ ! -d ${cachedir}/sledovanitv  ]; then
+	mkdir -p ${cachedir}/sledovanitv
+fi
 
 # Nacti playlist
 CAPABILITIES=$(jq -r '.capabilities // "h265,adaptive"' < ${configfile})
@@ -21,19 +40,20 @@ fi
 
 # ulozeni definic
 for def in $(echo $playlist | jq -r  '.channels | to_entries[] | select ((.value.locked=="none" or .value.locked=="'${lockedpin}'") and .value.type=="tv") | "\(.value.id)#\(.value.url)"'); do
-	filename=${cachedir}/sledovanitv_$(echo ${def} | cut -d# -f1)
+	filename=${cachedir}/sledovanitv/$(echo ${def} | cut -d# -f1)
 
 	url=$(echo ${def} | cut -d'#' -f2)
 	echo ${url} > ${filename}
 done
 
-# Vytvarim novy playlist
-echo "#EXTM3U"
-echo $playlist | jq -r  '.channels | to_entries[] | select ((.value.locked=="none" or .value.locked=="'${lockedpin}'") and .value.type=="tv") | "#EXTINF:-1 tvg-chno=\"\(.key+1)\" tvg-id=\"\(.value.id)\" epg-id=\"\(.value.id)\" tvg-name=\"\(.value.name)\" tvg-logo=\"\(.value.logoUrl)\"  group-title=\"${SLEDOVANITVGRP\(.value.group)}\",\(.value.name)\npipe://'$(dirname $(realpath $0) )'/sledovanitv-playback.sh \"'${cachedir}/sledovanitv_'\(.value.id)\" \"\(.value.name)\""' > ${FILETMP}
-sed -i -E 's/["#&()]/\\\\\0/g' ${FILETMP}
+# Vytvarim novy playlis
+echo $playlist | jq -r  '.channels | to_entries[] | select ((.value.locked=="none" or .value.locked=="'${lockedpin}'") and .value.type=="tv") | "#EXTINF:-1 tvg-chno=\"\(.key+1)\" tvg-id=\"\(.value.id)\" epg-id=\"\(.value.id)\" tvg-name=\"\(.value.name)\" tvg-logo=\"\(.value.logoUrl)\"  group-title=\"${SLEDOVANITVGRP\(.value.group)}\",\(.value.name)\npipe://'$(dirname $(realpath $0) )'/sledovanitv-playback.sh \"'${cachedir}/sledovanitv/'\(.value.id)\" \"\(.value.name)\""' > ${FILETMP}_tmp
+
+sed -i -E 's/["#&()]/\\\\\0/g' ${FILETMP}_tmp
 
 # Vypis playlist a nahrad v nem nazvy skupin
-while read; do eval echo -e ${REPLY}; done < ${FILETMP}
+while read; do eval echo -e ${REPLY}; done <${FILETMP}_tmp >>${FILETMP}
 
-rm ${FILETMP}
+cat ${FILETMP}
 
+rm ${FILETMP}_tmp
